d65f1cc524641dce05746074ee4aca9aec38b5af
koberlag <koberlag@gmail.com>
2015-08-09 01:49:17 -0500

    Setup functions to call bands in town api v1 and v2, in order to get additional data

--- js/app.js
+++ js/app.js
@@ -1,49 +1,97 @@
-var autocomplete;
+var autocomplete, lat, lng;
 $(function(){
     $( "#from-date" ).datepicker();
 	$(".filter-form").submit(function(){
 		$(".result-container").html("");
+		  // Get the place details from the autocomplete object.
+  		var place = autocomplete.getPlace();
 		getEvents();
+		// getLocation();
 	});
 
-	 // Create the autocomplete object, restricting the search
-  // to geographical location types.
-  	autocomplete = new google.maps.places.Autocomplete(
-      /** @type {HTMLInputElement} */(document.getElementById('location')),
-      { types: ['geocode'] });
-  	
+	initialize();
 });
 
+function initialize(){
+	// // Create the autocomplete object, restricting the search
+ //  	// to geographical location types.
+ //  	autocomplete = new google.maps.places.Autocomplete($('#location'), 
+ //  	{ 
+ //  		types: ['geocode'] 
+ //  	});
+ // Create the autocomplete object, restricting the search
+  // to geographical location types.
+  autocomplete = new google.maps.places.Autocomplete(
+      /** @type {HTMLInputElement} */(document.getElementById('location')),
+      { types: ['geocode'] });
+
+  	geolocate();
+  // When the user selects an address from the dropdown,
+  // populate the city and state in the location field
+  google.maps.event.addListener(autocomplete, 'place_changed', function() {
+     getEvents();
+  });
+}
+
 function getEvents(){
-	// the parameters we need to pass in our request to StackOverflow's API
-	 var request = {
+	// the parameters we need to pass in our request to the bands in town API
+	 var radius = $("#proximity-list").val(),
+	 request = {
 	 	format : 'json',
-	 	location : $("#location").val(),
-	 	app_id : 'Proximity'
+	 	location : lat + "," + lng,
+	 	radius: radius,
+	 	app_id : 'Proximity',
+	 	per_page:3
 	 },
-	 	eventUrl = "http://api.bandsintown.com/events/search";
+	 eventUrl = "http://api.bandsintown.com/events/search";
 	
-	var eventData = $.ajax({
-		url: eventUrl,
-		data: request,
-		dataType: "jsonp",
-		type: "GET",
-		})
+	getAJAX(request, eventUrl, "jsonp")
 	.done(function(eventData){
-		// var searchResults = showSearchResults(tag, result.items.length);
-
-		// $('.search-results').html(searchResults);
-
 		$.each(eventData, function(i, eventResult) {
-			var resultView = getEventView(eventResult);
-			$('.result-container').append(resultView);
+			getArtistEvents(request, eventResult.id, eventResult.artists[0].name)
+			// var resultView = getEventView(eventResult);
+			// $('.result-container').append(resultView);
 		});
-	})
-	.fail(function(jqXHR, error, errorThrown){
-		var errorElem = showError(error);
-		$('.search-results').append(errorElem);
 	});
 }
+
+function getArtistEvents(request, eventId, artist){
+	// the parameters we need to pass in our request to the bands in town API
+	 var radius = $("#proximity-list").val(),
+	 eventUrl = "http://api.bandsintown.com/artists/name/events/search";
+	 request.artist_id = artist;
+	 request.api_version = "2.0";
+	getAJAX(request, eventUrl, "jsonp")
+	.done(function(eventData){
+		$.each(eventData, function(i, eventResult) {
+			if(eventId == eventResult.id){
+				var resultView = getEventView(eventResult);
+				$('.result-container').append(resultView);
+			}
+		});
+	});
+}
+//Get all events, get artist by event artist name/ get event where event matches first call
+
+function getAJAX(request, url, datatype){
+	return $.ajax({
+		url: url,
+		data: request,
+		dataType: datatype,
+		type: "GET",
+		})
+	.fail(function(jqXHR, error, errorThrown){
+		showError(error); 
+	});
+}
+
+// takes error string and turns it into displayable DOM element
+var showError = function(error){
+	var errorElem = $('.templates .error').clone();
+	var errorText = '<p>' + error + '</p>';
+	errorElem.append(errorText);
+	$('.result-container').append(errorElem);
+};
 
 function getEventView(result){
 	var eventContainer 	= $(".templates").find(".event-container").clone(),
@@ -53,7 +101,8 @@ function getEventView(result){
 		venueState 		= eventContainer.find(".venue-state"),
 		artistList		= eventContainer.find(".artist-list");
 
-	eventLink.attr("href", result.url);
+	eventLink.attr("href", result.facebook_rsvp_url);
+	eventLink.text(result.title);
 	venueName.text(result.venue.name);
 	venueCity.text(result.venue.city);
 	venueState.text(result.venue.region);
@@ -62,43 +111,45 @@ function getEventView(result){
 		 var artistItem = "<li>" + artist.name;
 		 artistList.append(artistItem);
 	});
+			var x = result.artists[0],
+				imageUrl = x.thumb_url;
+
+	eventContainer.css("background-image", 'url(' + imageUrl + ')');  
+
 	return eventContainer;
 }
 
-function geolocate() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-      var geolocation = new google.maps.LatLng(
-          position.coords.latitude, position.coords.longitude);
-      var circle = new google.maps.Circle({
-        center: geolocation,
-        radius: position.coords.accuracy
-      });
-      autocomplete.setBounds(circle.getBounds());
-    });
-  }
-}
+// function geolocate() {
+//   if (navigator.geolocation) {
+//     navigator.geolocation.getCurrentPosition(function(position) {
+//       var geolocation = new google.maps.LatLng(
+//           position.coords.latitude, position.coords.longitude);
+//       var circle = new google.maps.Circle({
+//         center: geolocation,
+//         radius: position.coords.accuracy
+//       });
+//       getLocationName(geolocation);
+//       autocomplete.setBounds(circle.getBounds());
+//     });
+//   }
+// }
 
-function codeLatLng() {
-	var x = autocomplete;
-  var input = document.getElementById('latlng').value;
-  var latlngStr = input.split(',', 2);
-  var latlng = new google.maps.LatLng(latlngStr[0], latlngStr[1]);
-  geocoder.geocode({'location': latlng}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      if (results[1]) {
-        map.setZoom(11);
-        marker = new google.maps.Marker({
-          position: latlng,
-          map: map
-        });
-        infowindow.setContent(results[1].formatted_address);
-        infowindow.open(map, marker);
-      } else {
-        window.alert('No results found');
-      }
-    } else {
-      window.alert('Geocoder failed due to: ' + status);
-    }
-  });
+//Gets the current location and displays the city and state
+function geolocate(){
+	navigator.geolocation.getCurrentPosition(function (position) {
+	    var geocoder = new google.maps.Geocoder();
+	     lat = position.coords.latitude;
+	     lng = position.coords.longitude;
+	    var latlng = new google.maps.LatLng(lat, lng);
+
+	    getEvents(lat, lng);
+	    //reverse geocode the coordinates, returning location information.
+	    geocoder.geocode({ 'latLng': latlng }, function (results, status) {
+	    	if (status == google.maps.GeocoderStatus.OK) {
+	    		var address = results[3].address_components,
+		    		locationName = address[0].long_name + ", " + address[2].short_name;
+		    	$("#location").val(locationName);
+			}
+	    });
+	});
 }
